5f5eefce80497e2f1ba1424424f715e6805fe928
mentor-oonuki <mentor.oonuki@gmail.com>
2017-07-11 09:28:24 +0900

    【初期登録】 SoundManager サンプル

---
+++ Assets/Scripts/SoundManagerTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerTest : MonoBehaviour {
+
+	[SerializeField]
+	private int Number = 0;
+
+	public void OnClick()
+	{
+		SoundManager.Instance.Play(Number);
+	}
+
+}
